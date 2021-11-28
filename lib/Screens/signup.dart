04bd3129f1a9d2577da76eb85329c9bca3107ee3
Muhammad Javed <javedmughal609@gmail.com>
2021-11-27 20:20:41 -0800

    update signup page
    added back button in sign up page, and created outline of textfield.

--- lib/Screens/signup.dart
+++ lib/Screens/signup.dart
@@ -1,137 +1,152 @@
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
+import 'package:alert/alert.dart';
 
-class signup_page extends StatelessWidget {
+import 'login.dart';
+
+class SignUp extends StatefulWidget {
+  static String tag = 'Signup-page';
+  @override
+  _LoginPageState createState() => new _LoginPageState();
+}
+
+class _LoginPageState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Signup Profile",
-      home: signup(),
+    final logo = Hero(
+      tag: 'hero',
+      child: CircleAvatar(
+        backgroundColor: Colors.transparent,
+        radius: 48.0,
+        child: Image.asset('images/b-d-logo.png'),
+      ),
     );
-  }
-}
 
-class signup extends StatefulWidget {
-  @override
-  _EditProfilePageState createState() => _EditProfilePageState();
-}
-
-class _EditProfilePageState extends State<signup> {
-  bool showPassword = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-
-      body: Container(
-        padding: EdgeInsets.only(left: 16, top: 25, right: 16),
-        child: GestureDetector(
-          onTap: () {
-            FocusScope.of(context).unfocus();
-          },
-          child: ListView(
-            children: [
-              Text(
-                "SignUp",
-                style: TextStyle(
-                  fontSize: 35,
-                  fontWeight: FontWeight.w400,
-                  color: Colors.redAccent,
-                ),
-
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Center(
-                child: Stack(
-                  children: [
-                    Container(
-                      width: 130,
-                      height: 130,
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                            width: 4,
-                            color: Theme.of(context).scaffoldBackgroundColor,
-                          ),
-                          boxShadow: [
-                            BoxShadow(
-                                spreadRadius: 2,
-                                blurRadius: 10,
-                                color: Colors.redAccent),
-                          ],
-                          shape: BoxShape.circle,
-                          image: DecorationImage(
-                              fit: BoxFit.cover,
-                              image: AssetImage("image/2.jpg"
-                              ))),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              buildTextField("","Username", false),
-              buildTextField("", "E-mail", false),
-              buildTextField("", "Password", true),
-              buildTextField("", "Confirm Password", true),
-              SizedBox(
-                height: 15,
-              ),
-              RaisedButton(
-                  onPressed: () {},
-                  color: Colors.redAccent,
-                  padding: EdgeInsets.symmetric(horizontal: 50.0),
-
-                  elevation: 2,
-                  child: Text("Signup",
-                      style: TextStyle(
-                          fontFamily: 'Ephesis',
-                          fontWeight: FontWeight.w400,
-                          fontSize: 22,
-                          letterSpacing: 2.2,
-                          color: Colors.white))
-              ),
-
-            ],
-
+    final name = TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      //initialValue: 'abc@gmail.com',
+      decoration: InputDecoration(
+        hintText: 'Name',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0),
         ),
       ),
-    ),
     );
-  }
-
-  Widget buildTextField(
-      String labelText, String placeholder, bool isPasswordTextField) {
-    return Padding(
-        padding: const EdgeInsets.only(bottom: 35.0),
-        child: TextField(
-          obscureText: isPasswordTextField ? showPassword : false,
-          decoration: InputDecoration(
-            suffixIcon: isPasswordTextField
-                ? IconButton(
-                onPressed: () {
-                  setState(() {
-                    showPassword = !showPassword;
-                  });
-                },
-                icon: Icon(
-                  Icons.remove_red_eye,
-                  color: Colors.grey,
-                ))
-                : null,
-            contentPadding: EdgeInsets.only(bottom: 3),
-            labelText: labelText,
-            floatingLabelBehavior: FloatingLabelBehavior.always,
-            hintText: placeholder,
-            hintStyle: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-              color: Colors.black26,
+    final email = TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      initialValue: 'abc@gmail.com',
+      decoration: InputDecoration(
+        hintText: 'Email',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+    final password = TextFormField(
+      autofocus: false,
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Password',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+    final contact = TextFormField(
+      // autofocus: false,
+      //obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Conatct',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0),
+        ),
+      ),
+    );
+    final SigupButton = Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: RaisedButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+        ),
+        onPressed: () {
+          AlertDialog(title: Text("Thanks for joining"));
+        },
+        padding: EdgeInsets.all(12),
+        color: Colors.red,
+        child: Text('Sign Up', style: TextStyle(color: Colors.white,height: 2)),
+      ),
+    );
+    final loginLabel=  InkWell(
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => LoginPage()));
+      },
+      child: Container(
+        //margin: EdgeInsets.symmetric(vertical: 5),
+        //padding: EdgeInsets.all(15),
+        alignment: Alignment.bottomCenter,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'have an account ?',
+              style: TextStyle(fontSize: 13, fontWeight: FontWeight.w600),
             ),
+            SizedBox(
+              width: 10,
+            ),
+            Text(
+              'Login',
+              style: TextStyle(
+                  color: Colors.redAccent,
+                  fontSize: 13,
+                  fontWeight: FontWeight.w600),
+            ),
+          ],
+        ),
+      ),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.red,
+      body: Container(
+        padding: EdgeInsets.all(50.0),
+        decoration: BoxDecoration(
+          color: Colors.red,
+          //borderRadius: BorderRadius.all(Radius.circular(50)),
+        ),
+        child: Container(
+
+          padding: EdgeInsets.all(50.0),
+          decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(50),), color: Colors.white,),
+          child: ListView(
+            shrinkWrap: true,
+            //padding: EdgeInsets.only(left: 24.0, right: 24.0),
+            children: <Widget>[
+              IconButton(onPressed: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPage(),
+                  ),
+                );
+              }, icon: Icon(Icons.arrow_back_rounded,color: Colors.redAccent,)),
+              logo,
+              SizedBox(height: 10.0),
+              name,
+              SizedBox(height: 8.0),
+              email,
+              SizedBox(height: 8.0),
+              password,
+              SizedBox(height: 8.0),
+              contact,
+              SizedBox(height: 8.0),
+              SigupButton,
+              SizedBox(height: 8.0),
+              loginLabel,
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }

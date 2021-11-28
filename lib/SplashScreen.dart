9f9bd9daa7552d3c09b1e7f7e967225be5f75db9
Muhammad Javed <javedmughal609@gmail.com>
2021-11-27 23:41:08 -0800

    Update SplashScreen.dart

--- lib/SplashScreen.dart
+++ lib/SplashScreen.dart
@@ -7,6 +7,7 @@ import 'main.dart';
 void main() {
   return runApp(
     const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.redAccent,
         body: SplashScreen(),

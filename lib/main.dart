2976a5a8ebec635691e14aadcc5b2a9da6159d14
Muhammad Javed <javedmughal609@gmail.com>
2021-11-27 20:22:34 -0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'Screens/selection_screen.dart';
-void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -31,77 +30,66 @@ class MyHomePage extends StatelessWidget {
             Expanded(
                 flex: 3,
                 child: Container(
-                  width: double.infinity,
-                  decoration: const BoxDecoration(
-                    color: Colors.redAccent,
-                    borderRadius: BorderRadius.only(bottomRight: Radius.circular(70)),
-                  ),
-                  child: Container(
-                      decoration: const BoxDecoration(
-                          color: Colors.transparent,
-                          image: DecorationImage(
-                              image:AssetImage('assets/images/launch.png'),
-                              //fit:BoxFit.cover
-                          ),
-                        // button text
-                      )
-                  )
-                  ),
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      border: Border.all(width: 0, color: Colors.red),
+                      borderRadius: BorderRadius.only(bottomRight: Radius.circular(70),
+                      ),
+                    ),
+                    child: Image(image: AssetImage("images/launch.png"),height: 50,width: 50,))
             ),
             Expanded(
               flex: 2,
               child: Container(
                 //width: double.infinity,
-                color: Colors.redAccent,
+                color: Colors.red,
                 child: Container(
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(topLeft: Radius.circular(70)),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(15.0),
-                child: Column(
-                  children: [
-                   const Spacer(),
-                    const Text('Blood Donation',
-                     style: TextStyle(
-                       fontSize: 24,
-                       fontWeight: FontWeight.bold,
-                     ),
-                   ),
-                    const Text('is a great act\nof kindness.',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.black54,
-                        height: 1.5,
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
-                    ),
-                    ),
-                    const Spacer(),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 0, color: Colors.white),
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(70)),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Column(
                       children: [
-                      MaterialButton(
-                        height: 50,
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
-                        color: Colors.redAccent,
-                        onPressed: (){
-                          Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => const screen1()),
-                        );},
-                      child: const Text('Let Started',style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),),),
-                    ],)
-                  ],
+                        const Spacer(),
+                        const Text('Blood Donation',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const Text('is a great act\nof kindness.',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.black54,
+                            height: 1.5,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        const Spacer(),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            MaterialButton(
+                              height: 50,
+                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(70.0)),
+                              color: Colors.redAccent,
+                              onPressed: (){
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => const screen1()),
+                                );},
+                              child: Icon(Icons.arrow_forward, color: Colors.white,),),
+                          ],)
+                      ],
+                    ),
+                  ),
                 ),
-               ),
               ),
-            ),
             ),
           ],
         ),

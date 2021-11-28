5e3b75c0208410f6236a9ed86f8423a5670f04a3
Muhammad Javed <javedmughal609@gmail.com>
2021-11-27 23:48:40 -0800

    Update signup.dart

--- lib/Screens/signup.dart
+++ lib/Screens/signup.dart
@@ -6,8 +6,10 @@ import 'login.dart';
 
 class SignUp extends StatefulWidget {
   static String tag = 'Signup-page';
+
+  const SignUp({Key? key}) : super(key: key);
   @override
-  _LoginPageState createState() => new _LoginPageState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<SignUp> {
@@ -28,7 +30,7 @@ class _LoginPageState extends State<SignUp> {
       //initialValue: 'abc@gmail.com',
       decoration: InputDecoration(
         hintText: 'Name',
-        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        contentPadding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0),
         ),
       ),
@@ -39,7 +41,7 @@ class _LoginPageState extends State<SignUp> {
       initialValue: 'abc@gmail.com',
       decoration: InputDecoration(
         hintText: 'Email',
-        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        contentPadding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
       ),
     );
@@ -48,7 +50,7 @@ class _LoginPageState extends State<SignUp> {
       obscureText: true,
       decoration: InputDecoration(
         hintText: 'Password',
-        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        contentPadding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
       ),
     );
@@ -57,29 +59,29 @@ class _LoginPageState extends State<SignUp> {
       //obscureText: true,
       decoration: InputDecoration(
         hintText: 'Conatct',
-        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        contentPadding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0),
         ),
       ),
     );
     final SigupButton = Padding(
-      padding: EdgeInsets.symmetric(vertical: 16.0),
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
       child: RaisedButton(
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(24),
         ),
         onPressed: () {
-          AlertDialog(title: Text("Thanks for joining"));
+          const AlertDialog(title: Text("Thanks for joining"));
         },
-        padding: EdgeInsets.all(12),
+        padding: const EdgeInsets.all(12),
         color: Colors.red,
-        child: Text('Sign Up', style: TextStyle(color: Colors.white,height: 2)),
+        child: const Text('Sign Up', style: TextStyle(color: Colors.white,height: 2)),
       ),
     );
     final loginLabel=  InkWell(
       onTap: () {
         Navigator.push(
-            context, MaterialPageRoute(builder: (context) => LoginPage()));
+            context, MaterialPageRoute(builder: (context) => const LoginPage()));
       },
       child: Container(
         //margin: EdgeInsets.symmetric(vertical: 5),
@@ -87,7 +89,7 @@ class _LoginPageState extends State<SignUp> {
         alignment: Alignment.bottomCenter,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          children: const <Widget>[
             Text(
               'have an account ?',
               style: TextStyle(fontSize: 13, fontWeight: FontWeight.w600),
@@ -110,15 +112,15 @@ class _LoginPageState extends State<SignUp> {
     return Scaffold(
       backgroundColor: Colors.red,
       body: Container(
-        padding: EdgeInsets.all(50.0),
-        decoration: BoxDecoration(
+        padding: const EdgeInsets.all(50.0),
+        decoration: const BoxDecoration(
           color: Colors.red,
           //borderRadius: BorderRadius.all(Radius.circular(50)),
         ),
         child: Container(
 
-          padding: EdgeInsets.all(50.0),
-          decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(50),), color: Colors.white,),
+          padding: const EdgeInsets.all(50.0),
+          decoration: const BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(50),), color: Colors.white,),
           child: ListView(
             shrinkWrap: true,
             //padding: EdgeInsets.only(left: 24.0, right: 24.0),
@@ -126,22 +128,22 @@ class _LoginPageState extends State<SignUp> {
               IconButton(onPressed: (){
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => LoginPage(),
+                  MaterialPageRoute(builder: (context) => const LoginPage(),
                   ),
                 );
-              }, icon: Icon(Icons.arrow_back_rounded,color: Colors.redAccent,)),
+              }, icon: const Icon(Icons.arrow_back_rounded,color: Colors.redAccent,)),
               logo,
-              SizedBox(height: 10.0),
+              const SizedBox(height: 10.0),
               name,
-              SizedBox(height: 8.0),
+              const SizedBox(height: 8.0),
               email,
-              SizedBox(height: 8.0),
+              const SizedBox(height: 8.0),
               password,
-              SizedBox(height: 8.0),
+              const SizedBox(height: 8.0),
               contact,
-              SizedBox(height: 8.0),
+              const SizedBox(height: 8.0),
               SigupButton,
-              SizedBox(height: 8.0),
+              const SizedBox(height: 8.0),
               loginLabel,
             ],
           ),

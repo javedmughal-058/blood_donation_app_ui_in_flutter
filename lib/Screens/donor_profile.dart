d719807db6d9e6366e77de42142f1de323cca92c
Muhammad Javed <javedmughal609@gmail.com>
2021-11-28 23:24:55 -0800

    Update donor_profile.dart

--- lib/Screens/donor_profile.dart
+++ lib/Screens/donor_profile.dart
@@ -9,34 +9,32 @@ class Donor_profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Profile'),backgroundColor: Colors.red,),
+      appBar: AppBar(title: const Text('Profile'),backgroundColor: Colors.red,),
       body: Column(
         children: [
-          SizedBox(height: 10),
+          const SizedBox(height: 10),
           Expanded(
             flex: 2,
-          child: Container(
-            child: Row(
-              children: const [
-                CircleAvatar(
-                  maxRadius: 80,
-                  minRadius: 80,
-                  child: Image(
-                    image: AssetImage("images/man.png"),
-                  ),
+          child: Row(
+            children: const [
+              CircleAvatar(
+                maxRadius: 80,
+                minRadius: 80,
+                child: Image(
+                  image: AssetImage("images/man.png"),
                 ),
+              ),
 
-                Text('Donor Name',style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.normal,
-                  color: Colors.black54,
-                  fontFamily: 'Shrikhand',
-                ),),
-              ],
-            ),
+              Text('Donor Name',style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.normal,
+                color: Colors.black54,
+                fontFamily: 'Shrikhand',
+              ),),
+            ],
           ),
           ),
-          SizedBox(height: 20),
+          const SizedBox(height: 20),
           Expanded(flex: 8,
             child: Container(
               color: Colors.white,
@@ -55,12 +53,12 @@ class Donor_profile extends StatelessWidget {
                             context,
                             MaterialPageRoute(builder: (context) => const ChatScreen()),
                           );},
-                        child: Text('Call me',style: TextStyle(
+                        child: const Text('Call me',style: TextStyle(
                           fontSize: 16,
                           fontFamily: 'Shrikhand',
                           color: Colors.white,
                         ),),),
-                      SizedBox(width: 30),
+                      const SizedBox(width: 30),
                       MaterialButton(
                         height: 50,
                         minWidth: 70,
@@ -71,16 +69,16 @@ class Donor_profile extends StatelessWidget {
                             context,
                             MaterialPageRoute(builder: (context) => const ChatScreen()),
                           );},
-                        child: Text('Message',style: TextStyle(
+                        child: const Text('Message',style: TextStyle(
                           fontSize: 16,
                           fontFamily: 'Shrikhand',
                           color: Colors.white,
                         ),),),
                     ],
                   ),
-                  Divider(thickness: 1,color: Colors.black54,),
+                  const Divider(thickness: 1,color: Colors.black54,),
                   Row(
-                    children: [
+                    children: const [
                       Expanded(child:
                       Image(image: AssetImage("images/map.png")),
                       )

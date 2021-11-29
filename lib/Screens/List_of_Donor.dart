2e98ced4788af91af479859c9cb5e71634e8b2e8
Muhammad Javed <javedmughal609@gmail.com>
2021-11-28 20:56:22 -0800

    Update List_of_Donor.dart

--- lib/Screens/List_of_Donor.dart
+++ lib/Screens/List_of_Donor.dart
@@ -1,6 +1,8 @@
-
+import 'package:blood_bank/Screens/selection_screen.dart';
+import 'package:cool_alert/cool_alert.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'login.dart';
 
 class donorsList extends StatelessWidget {
   const donorsList({Key? key}) : super(key: key);
@@ -8,7 +10,138 @@ class donorsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Pak Blood Donation'),backgroundColor: Colors.redAccent,),
+      backgroundColor: Colors.red,
+      appBar: AppBar(title: Text('List of Donors'),backgroundColor: Colors.red,),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.red,
+              ),
+              child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Image(image: AssetImage("images/launch.png"),)
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Home',
+                style: TextStyle(
+                  color: Colors.black45,
+                  fontSize: 14.0,
+                ),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const screen1()),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.account_circle_rounded),
+              title: const Text('Admin',
+                style: TextStyle(
+                  color: Colors.black45,
+                  fontSize: 14.0,
+                ),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const LoginPage()),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.settings_applications_rounded ),
+              title: const Text('Setting',
+                style: TextStyle(
+                  color: Colors.black45,
+                  fontSize: 14.0,
+                ),),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.article_rounded),
+              title: const Text('About us',
+                style: TextStyle(
+                  color: Colors.black45,
+                  fontSize: 14.0,
+                ),
+              ),
+              onTap: () {
+                CoolAlert.show(
+                  context: context,
+                  type: CoolAlertType.info,
+                  text: 'Pak Blood Donation \n V0.1.1 \nCopyRight \'MultiLearner\'',
+                );
+
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.info),
+              title: const Text('App info',
+                style: TextStyle(
+                  color: Colors.black45,
+                  fontSize: 16.0,
+                ),),
+              onTap: () {
+                CoolAlert.show(
+                  context: context,
+                  type: CoolAlertType.info,
+                  text: 'Pak Blood Donation \n V0.1.1 \nCopyRight \'MultiLearner\'',
+                );
+              },
+            ),
+
+          ],
+        ),
+      ),
+      body:   SafeArea(child: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                color: Colors.red,
+              ),
+            ),
+
+          ),
+          Expanded(
+            flex: 9,
+            child: Container(
+              // width: double.infinity,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(50),topRight: Radius.circular(50), ),
+                  color: Colors.white,
+                ),
+              child: ListView(
+                  children: [
+                        Expanded(
+                          child: Row(
+                            children: [
+                                      
+                                  ]
+                                  ),
+                                 ),
+                             ]
+                            ),
+
+                        ),
+      ),
+      ]
+      ),
+      ),
     );
   }
 }

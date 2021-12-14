489247284db5c59dfd575eeb999950093d82eede
Muhammad Javed <91019922+javedmughal-058@users.noreply.github.com>
2021-12-14 18:04:18 +0500

    Update login.dart

--- lib/Screens/login.dart
+++ lib/Screens/login.dart
@@ -1,10 +1,8 @@
 import 'package:blood_bank/Screens/selection_screen.dart';
-import 'package:blood_bank/Screens/signup.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
   static String tag = 'Login-page';
-
   const LoginPage({Key? key}) : super(key: key);
   @override
   _LoginPageState createState() => _LoginPageState();
@@ -29,7 +27,7 @@ class _LoginPageState extends State<LoginPage> {
         hintText: 'Email',
         contentPadding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
           border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0),
-        ))
+        )),
     );
     final password = TextFormField(
       autofocus: false,
@@ -40,6 +38,7 @@ class _LoginPageState extends State<LoginPage> {
           border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0),
         )
       ),
+
     );
     final loginButton = Padding(
       padding: const EdgeInsets.symmetric(vertical: 16.0),
@@ -62,70 +61,54 @@ class _LoginPageState extends State<LoginPage> {
       ),
       onPressed: () {},
     );
-    final registerLabel=  InkWell(
-      onTap: () {
-        Navigator.push(
-            context, MaterialPageRoute(builder: (context) => const SignUp()));
-      },
-      child: Container(
-        //margin: EdgeInsets.symmetric(vertical: 20),
-        padding: const EdgeInsets.all(15),
-        alignment: Alignment.bottomCenter,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'Don\'t have an account ?',
-              style: TextStyle(fontSize: 13, fontWeight: FontWeight.w600),
-            ),
-            SizedBox(
-              width: 3,
-            ),
-            Text(
-              'Register',
-              style: TextStyle(
-                  color: Colors.redAccent,
-                  fontSize: 13,
-                  fontWeight: FontWeight.w600),
-            ),
-          ],
-        ),
-      ),
-    );
+
     return Scaffold(
       backgroundColor: Colors.red,
       body: Container(
-        padding: const EdgeInsets.all(50.0),
+        padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
         decoration: const BoxDecoration(
           color: Colors.red,
           //borderRadius: BorderRadius.all(Radius.circular(50)),
         ),
-        child: Container(
+        child: Center(
+          child: Form(
+            child: Container(
+              padding: const EdgeInsets.all(15.0),
+              decoration: BoxDecoration (
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(50),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.white.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: Offset(0, 3), // changes position of shadow
+                  ),
+                ],
+              ),
+              child: ListView(
 
-          padding: const EdgeInsets.all(50.0),
-          decoration: const BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(50),), color: Colors.white,),
-          child: ListView(
-
-            shrinkWrap: true,
-            //padding: EdgeInsets.only(left: 24.0, right: 24.0),
-            children: <Widget>[
-              IconButton(onPressed: (){
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const screen1(),
-                    ),
-                );
-              }, icon: const Icon(Icons.arrow_back_rounded,color: Colors.red,)),
-              logo,
-              const SizedBox(height: 12.0),
-              email,
-              const SizedBox(height: 8.0),
-              password,
-              const SizedBox(height: 10.0),
-              loginButton,
-              forgotLabel,
-              registerLabel
-            ],
+                shrinkWrap: true,
+                //padding: EdgeInsets.only(left: 24.0, right: 24.0),
+                children: <Widget>[
+                  IconButton(onPressed: (){
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const screen1(),
+                        ),
+                    );
+                  }, icon: const Icon(Icons.arrow_back_rounded,color: Colors.red,)),
+                  logo,
+                  const SizedBox(height: 12.0),
+                  email,
+                  const SizedBox(height: 8.0),
+                  password,
+                  const SizedBox(height: 10.0),
+                  loginButton,
+                  forgotLabel,
+                ],
+              ),
+            ),
           ),
         ),
       ),
